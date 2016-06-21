53ae2b6073e9c0500d430902b72bc30e3b9c7a14
Levente Laszlo <wandererc@gmail.com>
2016-06-22 00:41:59 +0200

    add 'new game' button

--- color.js
+++ color.js
@@ -5,6 +5,18 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+	colors = generateRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+	}
+	h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,6 +30,7 @@ for(var i = 0; i < squares.length; i++) {
 		console.log(pickedColor);
 		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play again?"
 			changeColor(clickedColor);
 			h1.style.background = clickedColor;
 		} else {
