8f692471ff111cd879169fc6b1c3c880f68bca06
Levente Laszlo <wandererc@gmail.com>
2016-06-25 10:04:58 +0200

    add easy game mode with just 3 colors

--- color.js
+++ color.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -6,9 +7,45 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function(){
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+	h1.style.background = "#232323";
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+		if(colors[i]) {
+			squares[i].style.background = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+
+hardBtn.addEventListener("click", function(){
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+	h1.style.background = "#232323";
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+			squares[i].style.background = colors[i];
+			squares[i].style.display = "block";
+	}
+});
+
 
 resetButton.addEventListener("click", function(){
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 
@@ -18,7 +55,9 @@ resetButton.addEventListener("click", function(){
 	h1.style.background = "#232323";
 });
 
+
 colorDisplay.textContent = pickedColor;
+
 
 for(var i = 0; i < squares.length; i++) {
 	// add initial colors
@@ -40,16 +79,19 @@ for(var i = 0; i < squares.length; i++) {
 	});
 }
 
+
 function changeColor(color){
 	for(var i = 0; i < squares.length; i++){
 		squares[i].style.background = color;
 	}
 }
 
+
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
+
 
 function generateRandomColors(num) {
 	var arr = [];
@@ -58,6 +100,7 @@ function generateRandomColors(num) {
 	}
 	return arr;	
 }
+
 
 function randomColor(){
 	var r = Math.floor(Math.random() * 256);
